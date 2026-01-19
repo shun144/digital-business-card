d6b373fe255b00ab794c778c487e543268ec8d49
shun144 <noelgld13@gmail.com>
2026-01-20 08:32:08 +0900

    sample.spce.tsx削除

--- src/lib/supabase/supabaseFunction.ts
+++ src/lib/supabase/supabaseFunction.ts
@@ -12,12 +12,12 @@ export const fetchAllUsers = async (): Promise<Tables<"users">[]> => {
 };
 
 export const fetchUser = async (userId: string) => {
-  // const res = await supabase.from("users").select("*").eq("id", userId);
+  const res = await supabase.from("users").select("*").eq("id", userId);
 
-  const res = await supabase
-    .from("users")
-    .select("name, user_skill (skill_id)")
-    .eq("id", userId);
+  // const res = await supabase
+  //   .from("users")
+  //   .select("name, user_skill (skill_id)")
+  //   .eq("id", userId);
 
   if (res.error) {
     throw new Error(res.error.message);
