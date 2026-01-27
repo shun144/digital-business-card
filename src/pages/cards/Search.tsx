6a42b602ecf56b4e7c812330857577a1ba7c2fa0
shun144 <noelgld13@gmail.com>
2026-01-27 18:35:12 +0900

    レイアウト修正

--- src/pages/cards/Search.tsx
+++ src/pages/cards/Search.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Field, Heading, Stack, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Field,
+  Heading,
+  Stack,
+  Input,
+  HStack,
+} from "@chakra-ui/react";
 import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
@@ -31,37 +39,49 @@ const Search = () => {
   return (
     <Stack align={"center"} justify={"center"} px="24px" py="48px">
       <Heading>名刺検索</Heading>
-      <Box w="250px" p="24px" bg="white" rounded={"sm"} shadow={"md"}>
+      <Stack
+        minW="350px"
+        minH="200px"
+        p="24px"
+        bg="white"
+        rounded={"sm"}
+        shadow={"md"}
+        justify={"space-between"}
+      >
         <form id="search-card" onSubmit={onSubmit}>
-          <Field.Root
-            invalid={!!errors.searchId}
-            flexDirection={"row"}
-            alignItems={"center"}
-          >
-            <Field.Label htmlFor="searchId">ID</Field.Label>
-            <Input
-              id="searchId"
-              placeholder="coffee"
-              {...register("searchId", {
-                required: "ユーザIDを入力してください",
-              })}
-            />
-            <Field.ErrorText>
-              {errors.searchId && errors.searchId.message}
-            </Field.ErrorText>
-          </Field.Root>
-          <Button
-            type="submit"
-            form="search-card"
-            variant="solid"
-            loading={isSubmitting}
-          >
-            検索
-          </Button>
+          <HStack>
+            <Field.Root
+              invalid={!!errors.searchId}
+              flexDirection={"row"}
+              alignItems={"center"}
+            >
+              <Field.Label htmlFor="searchId" fontSize={"md"}>
+                ID
+              </Field.Label>
+              <Input
+                id="searchId"
+                placeholder="coffee"
+                {...register("searchId", {
+                  required: "ユーザIDを入力してください",
+                })}
+              />
+              <Field.ErrorText>
+                {errors.searchId && errors.searchId.message}
+              </Field.ErrorText>
+            </Field.Root>
+            <Button
+              type="submit"
+              form="search-card"
+              variant="solid"
+              loading={isSubmitting}
+            >
+              検索
+            </Button>
+          </HStack>
         </form>
 
         <Button onClick={clickToRegister}>新規登録はこちら</Button>
-      </Box>
+      </Stack>
     </Stack>
   );
 };
